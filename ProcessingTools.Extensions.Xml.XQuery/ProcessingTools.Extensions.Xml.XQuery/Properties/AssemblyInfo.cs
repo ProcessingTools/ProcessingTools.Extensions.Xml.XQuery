e9e82a5f6544ab7ea631da20153beaf89b847a24
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2020-02-18 16:24:08 +0200

    Initial commit

---
+++ ProcessingTools.Extensions.Xml.XQuery/ProcessingTools.Extensions.Xml.XQuery/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="AssemblyInfo.cs" company="ProcessingTools">
+// Copyright (c) 2019 ProcessingTools. All rights reserved.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProcessingTools.Extensions.Xml.XQuery")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ProcessingTools")]
+[assembly: AssemblyProduct("ProcessingTools.Extensions.Xml.XQuery")]
+[assembly: AssemblyCopyright("Copyright © 2019 ProcessingTools")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("15c673af-40bd-43e9-aee9-1a819ac6145c")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
